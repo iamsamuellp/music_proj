2645c5e3ad18667aeba75726ebfaa3cc3773c6c3
Samuel McKnight <samuellp@gmail.comgit statusgit config --global user.user.email iamsamuellp@gmail.com>
2021-09-06 21:16:22 -0500

    fixed the put function had to erase code that was bring up an error.

--- music_project/music/views.py
+++ music_project/music/views.py
@@ -42,7 +42,7 @@ class SongDetail(APIView):
       serializer = SongSerializer(song, data=request.data)
       if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data)
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
 
     def delete (self,request,pk):

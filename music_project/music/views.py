298cc8d344c0e164cde2c27362495c572a4378dc
Samuel McKnight <samuellp@gmail.comgit statusgit config --global user.user.email iamsamuellp@gmail.com>
2021-09-06 20:34:06 -0500

    added functions and a class to get, put and delete.

--- music_project/music/views.py
+++ music_project/music/views.py
@@ -4,6 +4,8 @@ from .serializers import SongSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
+
 
 
 
@@ -21,3 +23,30 @@ class SongList(APIView):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+
+class SongDetail(APIView): 
+
+    def get_song(self, pk):
+      try:
+        return Song.objects.get(pk=pk)
+      except Song.DoesNotExist:  
+        raise
+
+    def get(self,request,pk):
+      song = self.get_song(pk)
+      serializers = SongSerializer(song)
+      return Response(serializers.data)
+
+    def put(self,request,pk):
+      song = self.get_song(pk)
+      serializer = SongSerializer(song, data=request.data)
+      if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  
+
+    def delete (self,request,pk):
+      song = self.get_song(pk)
+      song.delete()
+      return Response(status=status.HTTP_204_NO_CONTENT) 
+        
